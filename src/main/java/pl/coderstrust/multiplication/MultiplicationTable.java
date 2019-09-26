0ade5ff3f35f5cefe9d5e8654cac3b155594b50d
lapa44 <39618410+lapa44@users.noreply.github.com>
2019-09-26 11:13:27 +0200

    Apply review remarks.

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -3,51 +3,38 @@ import java.util.Scanner;
 
 public class MultiplicationTable {
 
-    private static int[][] multiplicationTable;
-
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int input;
-
         do {
-            System.out.print("Podaj wielkosc tabeli (max 12): ");
+            System.out.print("Please enter table's size (max 12): ");
             input = sc.nextInt();
-        } while(input > 12);
-
+        } while (input > 12);
         sc.close();
-        multiplicationTable = makeArray(input);
-        printMultiplicationTable(input);
-
+        int[][] multiplicationTable = makeArray(input);
+        printMultiplicationTable(input, multiplicationTable);
     }
 
-    private static int[][] makeArray(int size){
-
-        int[][] arr = new int[size + 1][size + 1];
-
-        for(int i = 1 ; i <= size ; i++){
-            arr[0][i] = i;
-            arr[i][0] = i;
-        }
-
-        for(int i = 1; i <= size ; i++){
-            for(int y = 1 ; y <= size ; y++){
-                arr[i][y] = arr[0][y] * arr[i][0];
+    private static int[][] makeArray(int size) {
+        int[][] arr = new int[size][size];
+        for (int i = 1; i <= size; i++) {
+            for (int y = 1; y <= size; y++) {
+                arr[i - 1][y - 1] = i * y;
             }
         }
-
         return arr;
     }
 
-    private static void printMultiplicationTable(int size){
-
-        for(int i = 0 ; i <= size ; i++){
-            for(int y = 0 ; y <= size ; y++){
-                if(i == 0 && y == 0){
-                    System.out.printf("%5s", "");
-                    continue;
-                }
-                System.out.printf("%5d", multiplicationTable[i][y]);
+    private static void printMultiplicationTable(int size, int[][] arr) {
+        System.out.printf("%5s", "");
+        for (int i = 1; i <= size; i++) {
+            System.out.printf("%5d", i);
+        }
+        System.out.println();
+        for (int i = 1; i <= size; i++) {
+            System.out.printf("%5d", i);
+            for (int y = 1; y <= size; y++) {
+                System.out.printf("%5d", arr[i - 1][y - 1]);
             }
             System.out.println();
         }
