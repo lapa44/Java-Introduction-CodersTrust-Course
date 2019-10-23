a16761b0648bc009d15cab2314123ee5de626bfb
lapa44 <39618410+lapa44@users.noreply.github.com>
2019-10-23 16:49:11 +0200

    Apply review remarks for FibonacciChecker v2.

--- src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
@@ -5,22 +5,20 @@ import java.util.Map;
 
 public class FibonacciChecker {
 
-    private static Map<Long, Boolean> numbersContainer = new HashMap<Long, Boolean>();
+    private static Map<Long, Boolean> cache = new HashMap<Long, Boolean>();
 
     public static boolean isFibonacciNumber(long number) {
         if (number < 0) {
             throw new IllegalArgumentException("Fibonacci numbers cannot be negative.");
         }
+        if (cache.containsKey(number)) {
+            return cache.get(number);
+        }
         long formula1 = 5 * (number * number) + 4;
         long formula2 = 5 * (number * number) - 4;
-        if (!numbersContainer.containsKey(number)) {
-            boolean condition = isPerfectSquare(formula1) || isPerfectSquare(formula2);
-            numbersContainer.put(number, condition);
-            return condition;
-        }
-        else {
-            return numbersContainer.get(number);
-        }
+        boolean result = isPerfectSquare(formula1) || isPerfectSquare(formula2);
+        cache.put(number, result);
+        return result;
     }
 
     private static boolean isPerfectSquare(long number) {
