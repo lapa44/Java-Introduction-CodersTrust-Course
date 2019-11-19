e0639f977ad8ec742c8f0bc0726653e1d3aaf462
lapa44 <39618410+lapa44@users.noreply.github.com>
2019-11-19 01:19:25 +0100

    Applied review remarks for regex.

--- src/main/java/pl/coderstrust/regex/IPv4Checker.java
+++ src/main/java/pl/coderstrust/regex/IPv4Checker.java
@@ -5,16 +5,15 @@ import java.util.regex.Pattern;
 
 class IPv4Checker {
 
-    private Pattern ipCompiledPattern;
+    private static final String IP_REGEXP = "^(25[0-5]\\.|" + "2[0-4]\\d\\.|" + "[0-1]?\\d{0,2}\\.){3}" +
+            "(25[0-5]|" + "2[0-4]\\d|" + "[0-1]?\\d{0,2})$";
+    private static Pattern pattern = Pattern.compile(IP_REGEXP);
 
-    IPv4Checker() {
-        String ipPattern = "^(25[0-5]\\.|" + "2[0-4]\\d\\.|" + "[0-1]?\\d{0,2}\\.){3}" +
-                            "(25[0-5]|" + "2[0-4]\\d|" + "[0-1]?\\d{0,2})$";
-        ipCompiledPattern = Pattern.compile(ipPattern);
-    }
-
-    boolean isApAdress(String input) {
-        Matcher ipMatcher = ipCompiledPattern.matcher(input);
+    public boolean isApAdress(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("IP Address cannot be equal to null.");
+        }
+        Matcher ipMatcher = pattern.matcher(input);
         return ipMatcher.matches();
     }
 }
--- src/test/java/pl/coderstrust/regex/IPv4CheckerTest.java
+++ src/test/java/pl/coderstrust/regex/IPv4CheckerTest.java
@@ -7,38 +7,57 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class IPv4CheckerTest {
 
     @ParameterizedTest
     @DisplayName("Parameterized test for invalid Ip Adresses")
     @ValueSource(strings = {"-1.0.0.0", "256.144.33.1000", "0.0.0.-10", "192.168.256.420", "1000.1000.1000.1000.1000"})
-    void shouldNotAcceptProvidedAddresses(String input) {
+    void shouldReturnFalseForInvalidIpAddress(String input) {
         assertFalse(new IPv4Checker().isApAdress(input));
     }
 
-    @Test
+    @ParameterizedTest
+    @DisplayName("Parameterized test for valid Ip Adresses")
+    @ValueSource(strings = {"0.0.0.0", "255.255.255.255", "1.0.0.10", "192.168.255.220", "192.168.0.131"})
+    void shouldReturnTrueForValidIpAddress(String input) {
+        assertTrue(new IPv4Checker().isApAdress(input));
+    }
+
+    @ParameterizedTest
     @DisplayName("Smart test for valid ip addresses")
-    void shouldAcceptProvidedAddresses() {
+    @ValueSource(strings = {"%d.1.1.1", "1.%d.1.1", "1.1.%d.1", "1.1.1.%d"})
+    void smartTest(String ipAddressTemplate) {
         for (int i = 0; i < 256; i++) {
-            assertTrue(new IPv4Checker().isApAdress(i+".1.1.1"));
+            assertTrue(new IPv4Checker().isApAdress(String.format(ipAddressTemplate, i)));
         }
+    }
+
+    @Test
+    @DisplayName("Unit test throwing illegal argument exception as input")
+    void shouldThrowIllegalArgumentExceptionForInput() {
+        assertThrows(IllegalArgumentException.class, () -> new IPv4Checker().isApAdress(null));
     }
 
     @Disabled
     /*
-    Time that this test took: 2 057 745 ms == 34,3 minutes.
+    Time that this test took using String: 2 057 745 ms == 34,3 minutes.
+    Time that this test took using StringBuilder: 1 727 192 ms = 28,8 minutes.
      */
     @Test
     @DisplayName("Unit test for all valid IP Addresses")
     void shouldAcceptAllValidAddresses() {
         IPv4Checker ipChecker = new IPv4Checker();
+        StringBuilder pattern = new StringBuilder();
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < 256; i++) {
-            for (int y = 0; y < 256; y++) {
-                for (int j = 0; j < 256; j++) {
-                    for (int k = 0; k < 256; k++) {
-                        assertTrue(ipChecker.isApAdress(i + "." + y + "." + j + "." + k));
+            for (int j = 0; j < 256; j++) {
+                for (int k = 0; k < 256; k++) {
+                    for (int l = 0; l < 256; l++) {
+                        pattern.append(i).append(".").append(j).append(".").append(k).append(".").append(l);
+                        assertTrue(ipChecker.isApAdress(pattern.toString()));
+                        pattern.setLength(0);
                     }
                 }
             }
