c5e38c230e51e5c18560edbcd7ec54ed2f7f6c33
lapa44 <39618410+lapa44@users.noreply.github.com>
2019-11-21 12:11:05 +0100

    Applied review remarks for regex v2.

--- src/main/java/pl/coderstrust/regex/IPv4Checker.java
+++ src/main/java/pl/coderstrust/regex/IPv4Checker.java
@@ -5,15 +5,14 @@ import java.util.regex.Pattern;
 
 class IPv4Checker {
 
-    private static final String IP_REGEXP = "^(25[0-5]\\.|" + "2[0-4]\\d\\.|" + "[0-1]?\\d{0,2}\\.){3}" +
-            "(25[0-5]|" + "2[0-4]\\d|" + "[0-1]?\\d{0,2})$";
+    private static final String IP_REGEXP = "^(25[0-5]\\.|2[0-4]\\d\\.|[0-1]?\\d{0,2}\\.){3}(25[0-5]|2[0-4]\\d|[0-1]?\\d{0,2})$";
     private static Pattern pattern = Pattern.compile(IP_REGEXP);
 
-    public boolean isApAdress(String input) {
+    public boolean isIPAdress(String input) {
         if (input == null) {
-            throw new IllegalArgumentException("IP Address cannot be equal to null.");
+            throw new IllegalArgumentException("Input cannot be null.");
         }
-        Matcher ipMatcher = pattern.matcher(input);
-        return ipMatcher.matches();
+        Matcher Matcher = pattern.matcher(input);
+        return Matcher.matches();
     }
 }
