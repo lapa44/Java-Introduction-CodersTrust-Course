c40cb1037a299f1d4c05f3908021f34cacc9e20d
lapa44 <39618410+lapa44@users.noreply.github.com>
2019-11-25 18:37:31 +0100

    Applied review remarks for regex v4.

--- src/main/java/pl/coderstrust/regex/IPv4Checker.java
+++ src/main/java/pl/coderstrust/regex/IPv4Checker.java
@@ -8,11 +8,11 @@ class IPv4Checker {
     private static final String IP_REGEXP = "^(25[0-5]\\.|2[0-4]\\d\\.|[0-1]?\\d{0,2}\\.){3}(25[0-5]|2[0-4]\\d|[0-1]?\\d{0,2})$";
     private static final Pattern PATTERN = Pattern.compile(IP_REGEXP);
 
-    public boolean isIpAdress(String input) {
+    public boolean isIpAddress(String input) {
         if (input == null) {
             throw new IllegalArgumentException("Input cannot be null.");
         }
-        Matcher Matcher = PATTERN.matcher(input);
-        return Matcher.matches();
+        Matcher matcher = PATTERN.matcher(input);
+        return matcher.matches();
     }
 }
