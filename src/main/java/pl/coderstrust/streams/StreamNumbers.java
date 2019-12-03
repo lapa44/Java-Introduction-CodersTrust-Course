4934028e1e96e8047131b4c85389f68c10566310
lapa44 <39618410+lapa44@users.noreply.github.com>
2019-12-03 11:17:31 +0100

    Applied review remarks for streams.

--- src/main/java/pl/coderstrust/streams/StreamNumbers.java
+++ src/main/java/pl/coderstrust/streams/StreamNumbers.java
@@ -1,37 +1,34 @@
 package pl.coderstrust.streams;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.PrintWriter;
 import java.util.Arrays;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamNumbers {
 
-    private static final Path INPUT_PATH = Paths.get("src/main/resources/numbersInput.txt");
-    private static final Path OUTPUT_PATH = Paths.get("src/main/resources/numbersOutput.txt");
+    private static final String INPUT_PATH = "src/main/resources/numbersInput.txt";
+    private static final String OUTPUT_PATH = "src/main/resources/numbersOutput.txt";
     private static final String NUMBERS_PATTERN = "^[-?\\d\\s]+$";
 
     public static void main(String[] args) throws IOException {
-        Stream<String> inputFromFile = Files.lines(INPUT_PATH)
-                .filter(s -> s.matches(NUMBERS_PATTERN))
-                .map(s -> {
-                    s = deleteWhitespacesToFirstDigit(s);
-                    int e = Arrays.stream(s.split("\\s+")).mapToInt(Integer::valueOf).sum();
-                    s = Arrays.stream(s.split("\\s+")).reduce("", (s1, s2) -> s1 + s2 + "+");
-                    return s.substring(0, s.length() - 1)
-                            + "=" + e;
-                    });
-        Files.write(OUTPUT_PATH, inputFromFile.collect(Collectors.toList()));
+        try (PrintWriter writer = new PrintWriter(new FileWriter(OUTPUT_PATH));
+             Stream<String> lines = new BufferedReader(new FileReader(INPUT_PATH)).lines()) {
+                    lines.filter(StreamNumbers::isLineValid)
+                    .map(line -> line.trim().split("\\s+"))
+                    .map(arrayOfNumbers -> {
+                        int sum = Arrays.stream(arrayOfNumbers).mapToInt(Integer::parseInt).sum();
+                        String numbers = Arrays.stream(arrayOfNumbers).reduce((s1, s2) -> String.format("%s+%s", s1, s2)).get();
+                        return String.format("%s=%d", numbers, sum);
+                    })
+                    .forEach(writer::println);
+        }
     }
 
-    private static String deleteWhitespacesToFirstDigit(String s) {
-        int index = 0;
-        while (s.charAt(index) == ' ') {
-            index++;
-        }
-        return s.substring(index);
+    private static boolean isLineValid(String line) {
+        return line.matches(NUMBERS_PATTERN);
     }
 }
